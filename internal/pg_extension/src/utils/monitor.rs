e88883ac07d9a9721e9ce7e4ff9dcf8c024ef162
working <57171759+NLGithubWP@users.noreply.github.com>
2024-07-14 15:41:13 +0800

    Update

--- internal/pg_extension/src/utils/monitor.rs
+++ internal/pg_extension/src/utils/monitor.rs
@@ -3,22 +3,22 @@ use std::thread;
 use std::time::{Duration, Instant};
 use sysinfo::{System, SystemExt, ProcessExt};
 
-pub fn start_memory_monitoring(interval: Duration, memory_log: &mut Vec<(String, f64, u64)>, label: &str, start_time: Instant) {
-    let pid = std::process::id() as i32;
-    let mut system = System::new_all();
-
-    thread::spawn(move || {
-        loop {
-            system.refresh_all();
-            if let Some(process) = system.process(pid) {
-                let memory_usage = process.memory();
-                let timestamp = start_time.elapsed().as_secs_f64(); // Use the same Instant to get elapsed time
-                memory_log.push((label.to_string(), timestamp, memory_usage)); // Append directly to the vector
-            }
-            thread::sleep(interval);
-        }
-    });
-}
+// pub fn start_memory_monitoring(interval: Duration, memory_log: &mut Vec<(String, f64, u64)>, label: &str, start_time: Instant) {
+//     let pid = std::process::id() as i32;
+//     let mut system = System::new_all();
+//
+//     thread::spawn(move || {
+//         loop {
+//             system.refresh_all();
+//             if let Some(process) = system.process(pid) {
+//                 let memory_usage = process.memory();
+//                 let timestamp = start_time.elapsed().as_secs_f64(); // Use the same Instant to get elapsed time
+//                 memory_log.push((label.to_string(), timestamp, memory_usage)); // Append directly to the vector
+//             }
+//             thread::sleep(interval);
+//         }
+//     });
+// }
 
 
 pub fn log_memory_usage(memory_log: &mut Vec<(String, f64, u64)>, start_time: Instant, label: &str) {
